0d077af3b2e49cc8d7341c094ff2a50172cd3940
novichenkoAlexander <a.novichenko1996@gmail.com>
2021-03-21 21:09:38 +0300

    made list of notes with recycleView

--- app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
@@ -14,40 +14,15 @@ class MainFragment : NavigationFragment<FragmentMainBinding>(R.layout.fragment_m
 
     private val viewModel: MainViewModel by viewModel()
 
-    var count: Int = 0
-        set(value) {
-            field = value
-            viewBinding.tvCount.text = field.toString()
-        }
-
-    var ink: Int = 0
-        set(value) {
-            field = value
-            viewBinding.tvCount2.text = field.toString()
-        }
-
-    var sum: Int = 0
-        set(value) {
-            field = value
-            viewBinding.tvCountSum.text = field.toString()
-        }
-
-    override fun onInsetsReceived(top: Int, bottom: Int, hasKeyboard: Boolean) {
-
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewBinding.btnClicker.setOnClickListener {
-            count++
-        }
-        viewBinding.btnClicker2.setOnClickListener {
-            ink++
-        }
-        viewBinding.btnSum.setOnClickListener {
-            sum = count + ink
-        }
+        viewBinding.recycleView.adapter = NotesRecyclerViewAdapter(viewModel.notes)
+
     }
 
-
+    override fun onInsetsReceived(top: Int, bottom: Int, hasKeyboard: Boolean) {
+        viewBinding.toolbar.setPadding(0, top, 0, 0)
+        viewBinding.recycleView.setPadding(0, 0, 0, bottom)
+    }
 }
--- app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
@@ -3,4 +3,23 @@ package io.techmeskills.an02onl_plannerapp.screen.main
 import io.techmeskills.an02onl_plannerapp.support.CoroutineViewModel
 
 class MainViewModel : CoroutineViewModel() {
+
+    val notes = listOf(
+        Note("Wake up!"),
+        Note("Stand up"),
+        Note("Brush your teeth"),
+        Note("Make breakfast"),
+        Note("Make tea"),
+        Note("Clean your head"),
+        Note("Make some exercise"),
+        Note("Read some pages of book"),
+        Note("Dress up"),
+        Note("And go to work")
+    )
+
 }
+
+class Note(
+    val title: String,
+    val date: String? = null
+)
