ca97858d135c42eb452cdcd1e8ba1d928e180a5c
novichenkoAlexander <a.novichenko1996@gmail.com>
2021-03-31 22:00:38 +0300

    Made adding notes in coroutine

--- app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
@@ -1,13 +1,30 @@
 package io.techmeskills.an02onl_plannerapp.screen.main
 
+import android.widget.EditText
+import androidx.lifecycle.MutableLiveData
 import io.techmeskills.an02onl_plannerapp.support.CoroutineViewModel
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainViewModel : CoroutineViewModel() {
 
-    var notes: ArrayList<Note> = ArrayList()
+    val listLiveData = MutableLiveData<List<Note>>()
+    private val listOfNotes = ArrayList<Note>()
+
+    fun addNoteToList(view: EditText) {
+        val note = Note(view.text.toString(), Date().toString())
+        launch {
+            if (note.title.isNotBlank() && note.title.isNotEmpty()) {
+                listOfNotes.add(note)
+            }
+            listLiveData.postValue(listOfNotes)
+        }
+    }
+
 }
 
 class Note(
     val title: String,
-    //val date: String? = null
+    val date: String
 )
