63f1ddd56a417495b2ff9878150f89c1265cff54
novichenkoAlexander <a.novichenko1996@gmail.com>
2021-04-13 21:02:51 +0300

    Added hideKeyboard option while clicked outside the editText

--- app/src/main/java/io/techmeskills/an02onl_plannerapp/MainActivity.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package io.techmeskills.an02onl_plannerapp
 
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -20,6 +23,15 @@ class MainActivity : SupportActivityInset<ActivityMainBinding>() {
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
         setWindowTransparency(this)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 
     override fun getActiveFragment(): Fragment? {
